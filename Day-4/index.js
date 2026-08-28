5b519501bcf4dd80c9f957ee8cbbce3488567396
Zia Uddin <ziauddinzoy@gmail.com>
2026-08-28 19:58:35 +0600

    completed day-4 all challenges

--- Day-4/index.js
+++ Day-4/index.js
@@ -1,27 +1,23 @@
-let price = 1000;
-let discount = 20;
-function calculateDiscount() {
+// 1. calculateDiscount
+function calculateDiscount(price, discount) {
     let finalPrice = price - (price * discount / 100);
     return finalPrice;
 
 }
 
-console.log('Final price:', calculateDiscount());
+console.log('Final price:', calculateDiscount(1200, 15));
 
 
-let burgerPrice = 500;
-let tax = 5; // 5%
-
-function calculateTax() {
+// 2.calculateTax() 
+function calculateTax(burgerPrice, tax) {
     let taxValue = burgerPrice * tax / 100;
     return taxValue;
 }
 
-console.log('Tax tk:', calculateTax());
+console.log('Tax tk:', calculateTax(500, 5));
 
-
-let grade = 69;
-function getGrade() {
+// 3.getGrade()
+function getGrade(grade) {
     if (grade >= 80) {
         console.log('A+');
     } else if (grade >= 70) {
@@ -41,9 +37,9 @@ function getGrade() {
     }
 }
 
-getGrade();
+getGrade(69);
 
-
+//4.isEven()
 let number = [1,2,3,4,5,6,7,8,9,10];
 let evenNum = [];
 
@@ -58,35 +54,94 @@ function isEven() {
 isEven();
 console.log('even num:', evenNum);
 
+//5.isPalindrome()
+function isPalindrome(text) {
+    let left = 0;
+    let right = text.length - 1;
 
-let word = 'madam';
-let real = [];
-let reverse = [];
+    while (left < right) {
+        if (text[left] !== text[right]) {
+            return 'No!'
+        }
 
-function isPalindrome() {
-    for (let i = 0; i < word.length; i++) {
-        real.push(word[i]);
+        left++;
+        right--;
     }
 
-    for (let i = word.length - 1; i >= 0; i--) {
-       reverse.push(word[i])
-    }
+    return 'This is palindrome'
+}
 
-    if (real === reverse) {
-        console.log('same');
+console.log(isPalindrome('madam'));
+console.log(isPalindrome('hello'));
+
+//6.findLargest()
+let lNumber = [10, 20, 40, 20, 45, 10];
+let largeNum = lNumber[0];
+function findLargest() {
+    for (let i = 0; i < lNumber.length; i++) {
+        if (lNumber[i] > largeNum) {
+            largeNum = lNumber[i]
+        }
+        // console.log(lNumber[i]);
+    }
+}
+
+findLargest();
+console.log(largeNum);
+
+//7.convertTemperature()
+function convertTemperature(celcius, fahrenheit) {
+    let fahr = (celcius * 9/5) + 32;  //celcius to fahrenheit
+    console.log('fahrenheit:', fahr.toFixed());
+    
+    let cel = (fahrenheit - 32) * 5/9; // fahrenheit to celcius
+    console.log('celcius:', cel.toFixed());
+    
+}
+
+convertTemperature(22, 78);
+
+// 8.calculateAge()
+
+function calculateAge(birthYear) {
+    const currentYear = new Date().getFullYear();
+    let finalAge = currentYear - birthYear;
+    console.log('finalAge:', finalAge);
+    
+}
+
+calculateAge(2002);
+
+//9.formatName()
+function formatName(firstName, lastName) {
+    let userName = firstName + lastName;
+    console.log('user name:', userName);
+    
+}
+
+formatName('Alfred', ' Josef')
+
+//10.calculateShipping()
+function calculateShipping(order) {
+    if (order >= 5000) {
+        console.log('Free Shipping');
+
+    } else if (order >= 2000) {
+        console.log('Shipping: ' + 60);
+
+    } else if (order >= 1000) {
+        console.log('Shipping: ' + 80);
+
     } else {
-        console.log('no');
+        console.log('Shipping: ' + 85);
         
     }
 }
 
-isPalindrome();
-console.log(real);
-console.log(reverse);
+calculateShipping(3000);
 
 
 let numS = [1,2,3,4,5,4, 3, 2,1]
-
 let seen = [];
 let duplicate = [];
 
@@ -98,36 +153,6 @@ for (let i = 0; i < numS.length; i++) {
     } else {
         seen.push(y)
     }
-
-    if (seen.includes(y) === discount.includes(y)) {
-        console.log('lol');
-        
-    } else {
-        console.log('chod');
-        
-    }
-
-    
 }
 
 console.log(duplicate);
-
-
-// for(let i = 5; i > 0; i--) {
-//     console.log(i);
-    
-// }
-
-
-// let user = 'abia';
-// for (let i = 0; i < user.length; i++) {
-//     console.log(user[i]);
-    
-// }
-// for (let i = user.length - 1; i >= 0; i--) {
-//     console.log(user[i]);
-    
-// }
-
-// let ll = 'ppp';
-// console.log(ll.includes());
